061a35dd5ebcdfc741c8b11d45f45bd7adfab83e
Theusma26 <matheusrodriguesoliveira258@gmail.com>
2024-08-26 20:10:28 -0300

    feat: create migration and entitie transaction

---
+++ backend/app/Domain/Transaction/Transaction.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Domain\Entities;
+
+class Transaction
+{
+    public $id;
+    public $description;
+    public $amount;
+    public $type;
+    public $categoria;
+    public $transaction_date;
+
+    public function __construct($id, $description, $amount, $type, $categoria, $transaction_date)
+    {
+        $this->id = $id;
+        $this->description = $description;
+        $this->amount = $amount;
+        $this->type = $type;
+        $this->categoria = $categoria;
+        $this->transaction_date = $transaction_date;
+    }
+}
